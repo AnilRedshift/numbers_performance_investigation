d2741e166cd7c1e9a379c4b534c9ff7423719964
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-04-05 23:57:01 -0700

    Add logical jump

--- Makefile
+++ Makefile
@@ -2,6 +2,11 @@ mathy: mathy.c
 	gcc -o mathy mathy.c -O3
 mathyasm: mathy.c
 	gcc -S -o mathy.s mathy.c
+
+logical: if_logical.c
+	gcc -o logical if_logical.c -O3
+logicalasm: if_logical.c
+	gcc -S -o logical.s if_logical.c
 .PHONY: clean
 clean:
 	rm ./mathy
---
+++ if_logical.c
@@ -0,0 +1,22 @@
+
+#include <stdlib.h>
+#include <math.h>
+
+int isEven(int num)
+{
+  return (num >= 10 && num <= 99) ||
+         (num >= 1000 && num <= 9999) ||
+         (num >= 100000 && num <= 999999) ||
+         (num >= 10000000 && num <= 99999999) ||
+         (num >= 1000000000 && num <= 9999999999);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    return -1;
+  }
+  int num = atoi(argv[1]);
+  return isEven(num);
+}
